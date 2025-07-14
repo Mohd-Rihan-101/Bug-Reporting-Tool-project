00d71b6d231ed8cfad27a0b67c49d36dde1633f5
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-07-14 11:36:30 +0530

    add login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,23 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const register = async (req,res)=>{
-    const {name , email, password , role} = req.body;
-    const hashed = await bcrypt.hash(password, 10);
-    const user = await User.create({name , email , hashed , role});
-    res.status(200).json({msg : "User register"});
+const register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, hashed, role });
+  res.status(200).json({ msg: "User register" });
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password)));
+  res.status(401).json({ msg: "invalid credential" });
+
+  const token = jwt.sign({ id : user._id, role : user.role}, process.env.jwt_SECRET);
+  res.json({token});
+};
+
+module.exports = {register , login};
 
