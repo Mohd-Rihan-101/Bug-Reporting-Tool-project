189f96369c3259b8a4ce4ae8b93150c1eadbc7c9
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-07-14 11:07:10 +0530

    model for user

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["user", "developer"],
+    default: "user",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 require('./db');
 
-app.listen(600, (req,res)=>{
+app.listen(6000, (req,res)=>{
     console.log("server is listening on PORT 6000");
 })
