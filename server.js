a2033ee66a2fd9ffa3db625b3e4187e03296165f
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-07-28 01:17:15 +0530

    add routes in server.js

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ dotenv.config();
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
+const issueRoutes = require("./routes/issueRoutes");
+
 
 app.use("/api/auth", authRoutes);
+app.use("/api/issues", issueRoutes);
 
 app.listen(6000, () => {
   console.log("server is listening on PORT 6000");
